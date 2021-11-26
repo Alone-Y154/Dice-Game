dced0eeaddd8ad791cf2c8ba980fa4c7d94a38bc
Yashwanth Krishna <72192888+Alone-Y154@users.noreply.github.com>
2021-11-26 23:08:23 +0530

    Added initial files

---
+++ index.js
@@ -0,0 +1,26 @@
+// gives a random number from 1 -- 6
+var randomNumber1 = Math.floor(Math.random()*6+1);
+
+
+// selects random images
+var randomDiceImage1 = "image\dice" + randomNumber1 + ".png";
+
+ document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage1);
+
+
+
+var randomNumber2 =  Math.floor(Math.random()*6+1);
+
+var randomDiceImage2 = "image\dice" + randomNumber2 + ".png";
+
+ document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML("🔥player 1 won");
+}
+  else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML("🔥player 2 won");
+  }
+else {
+  document.querySelector("h1").innerHTML("draw!");
+}
