2723bb1f9c3953774c8445c7e6d76302ff1be537
Yashwanth Krishna <72192888+Alone-Y154@users.noreply.github.com>
2021-11-27 22:32:44 +0530

    sorted minor errors

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
+// player 1
+
 // gives a random number from 1 -- 6
-var randomNumber1 = Math.floor(Math.random()*6+1);
+var randomNumber1 = Math.floor(Math.random() * 6 )+ 1;
 
 
 // selects random images
-var randomDiceImage1 = "image\dice" + randomNumber1 + ".png";
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
 
- document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage1);
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1);
 
 
+// player 2
 
-var randomNumber2 =  Math.floor(Math.random()*6+1);
 
-var randomDiceImage2 = "image\dice" + randomNumber2 + ".png";
+var randomNumber2 = Math.floor(Math.random() * 6 )+ 1;
 
- document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 
-if(randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML("🔥player 1 won");
-}
-  else if (randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML("🔥player 2 won");
-  }
-else {
-  document.querySelector("h1").innerHTML("draw!");
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+// conditions
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML =("🔥player 1 won");
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML =("🔥player 2 won");
+} else {
+  document.querySelector("h1").innerHTML =("draw!");
 }
